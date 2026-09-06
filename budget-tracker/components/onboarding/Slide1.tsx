59d65a559e4a2ea4f1ed188975a20600b4225c72
Alessio Galtelli <alessiogaltelli@yahoo.com>
2026-09-06 19:09:03 +0200

    addedo more field to user

--- budget-tracker/components/onboarding/Slide1.tsx
+++ budget-tracker/components/onboarding/Slide1.tsx
@@ -1,11 +1,20 @@
 "use client";
 import { User } from "@/types/User";
-import { Form, Input, Label, NumberField, TextField } from "@heroui/react";
+import {
+  Form,
+  Input,
+  Label,
+  NumberField,
+  TextField,
+  Select,
+  ListBox,
+} from "@heroui/react";
 type FirstSlideProps = {
   user: User;
   editUserField: <K extends keyof User>(key: K, value: User[K]) => void;
 };
-
+const STATUS = ["single", "in a relationship", "married"];
+const JOB = ["part-time", "indeterminated", "unemployed"];
 export default function FirstSlide({ user, editUserField }: FirstSlideProps) {
   return (
     <Form className="flex flex-col gap-4">
@@ -19,6 +28,42 @@ export default function FirstSlide({ user, editUserField }: FirstSlideProps) {
         />
       </TextField>
 
+      <Select variant="secondary">
+        <Label>Status</Label>
+        <Select.Trigger>
+          <Select.Value />
+          <Select.Indicator />
+        </Select.Trigger>
+        <Select.Popover>
+          <ListBox>
+            {STATUS.map((s, i) => (
+              <ListBox.Item id={s}>
+                {s}
+                <ListBox.ItemIndicator />
+              </ListBox.Item>
+            ))}
+          </ListBox>
+        </Select.Popover>
+      </Select>
+
+      <Select variant="secondary">
+        <Label>Job</Label>
+        <Select.Trigger>
+          <Select.Value />
+          <Select.Indicator />
+        </Select.Trigger>
+        <Select.Popover>
+          <ListBox>
+            {JOB.map((s, i) => (
+              <ListBox.Item id={s}>
+                {s}
+                <ListBox.ItemIndicator />
+              </ListBox.Item>
+            ))}
+          </ListBox>
+        </Select.Popover>
+      </Select>
+
       <NumberField
         variant="secondary"
         isRequired
@@ -27,6 +72,51 @@ export default function FirstSlide({ user, editUserField }: FirstSlideProps) {
         onChange={(value) => editUserField("annualIncome", value ?? 0)}
       >
         <Label>Income</Label>
+        <NumberField.Group>
+          <NumberField.DecrementButton />
+          <NumberField.Input />
+          <NumberField.IncrementButton />
+        </NumberField.Group>
+      </NumberField>
+
+      <NumberField
+        variant="secondary"
+        isRequired
+        name="annualIncome"
+        value={user.annualIncome}
+        onChange={(value) => editUserField("annualIncome", value ?? 0)}
+      >
+        <Label>Savings</Label>
+        <NumberField.Group>
+          <NumberField.DecrementButton />
+          <NumberField.Input />
+          <NumberField.IncrementButton />
+        </NumberField.Group>
+      </NumberField>
+
+      <NumberField
+        variant="secondary"
+        isRequired
+        name="annualIncome"
+        value={user.annualIncome}
+        onChange={(value) => editUserField("annualIncome", value ?? 0)}
+      >
+        <Label>Debt</Label>
+        <NumberField.Group>
+          <NumberField.DecrementButton />
+          <NumberField.Input />
+          <NumberField.IncrementButton />
+        </NumberField.Group>
+      </NumberField>
+
+      <NumberField
+        variant="secondary"
+        isRequired
+        name="annualIncome"
+        value={user.annualIncome}
+        onChange={(value) => editUserField("annualIncome", value ?? 0)}
+      >
+        <Label>Investments</Label>
         <NumberField.Group>
           <NumberField.DecrementButton />
           <NumberField.Input />
